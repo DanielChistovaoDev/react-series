5c93bee57b406c81a41aef0366603e55e10d812b
DanielChistovaoDev <contato.danielchistovao@gmail.com>
2020-06-28 04:24:21 -0300

    metodo de login e estilizacao

--- src/components/FormRow.js
+++ src/components/FormRow.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 
 const FormRow = props => {
-    const { children } = props;
+    const { children, first, last } = props;
 
     return (
-                <View style={styles.container}>
+                <View style={[styles.container, 
+                              first ? styles.first : null ,
+                              last ? styles.last : null]
+                            }>
                     { children } 
                 </View>
             )   
@@ -20,7 +23,13 @@ const styles = StyleSheet.create({
         marginTop: 5,
         marginBottom: 5,
         elevation: 5
-    }
+    },
+    first: {
+        marginTop: 10
+    },
+    last: {
+        marginBottom: 10
+     }
 });
 
 export default FormRow;
--- src/pages/LoginScreen.js
+++ src/pages/LoginScreen.js
@@ -1,32 +1,70 @@
 import React from 'react';
-import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
 import FormRow from '../components/FormRow';
 
 export default class LoginScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+    }
+
+    onChangeInput(field, value) {
+        this.setState({ [field]: value});
+    }
+
+    tryLogin() {
+        console.log('state ->', this.state);
+        
+    }
+
     render () {
         return (
-            <View>
-                <FormRow>
+            <View style={styles.container}>
+                <FormRow first>
                     <TextInput 
                         style={styles.input}
-                        placeholder='email/user'/>
+                        placeholder='email/user'
+                        value={this.state.email}
+                        onChangeText={value => this.onChangeInput('email', value) } />
                 </FormRow>
                 
-                <FormRow>
+                <FormRow last>
                     <TextInput  
                         style={styles.input}
                         placeholder='password'
-                        secureTextEntry={true} />
+                        secureTextEntry={true}
+                        value={this.state.password}
+                        onChangeText={value => this.onChangeInput('password', value) }/>
                 </FormRow>
+
+                        <Button 
+                            title='ENTRAR'
+                            onPress={ () => this.tryLogin()}
+                            />
+
+
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    container: {
+        paddingLeft: 10,
+        paddingRight: 10
+    },
     input: {
         paddingLeft: 5,
         paddingRight: 5,
         paddingBottom: 10
+    },
+    button: {
+        marginTop: 5
     }
 });
